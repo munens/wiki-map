d0cc4db6407c583ba5c4dced06f5a5741c25d6c1
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-07-13 19:47:01 +0000

    Add google map basic functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,15 @@ $(() => {
       }
     });;
 });
+
+$(document).ready(() => {
+  $('.map-image')
+});
+
+ function initMap() {
+        var mapDiv = document.getElementById('map-image');
+        var map = new google.maps.Map(mapDiv, {
+            center: {lat: 44.540, lng: -78.546},
+            zoom: 8
+        });
+      }
