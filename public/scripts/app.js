826a6466ad7dd977c69007783fb6e88d068af9b1
Munene Kaumbutho <Munene Kaumbutho>
2016-07-13 23:16:54 +0000

    Resized the map , implemented google maps with jquery and aplied scss features to it

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,14 +9,21 @@ $(() => {
     });;
 });
 
-$(document).ready(() => {
-  $('.map-image')
+$(document).ready( function(){
+
+  initMap();
+
 });
 
- function initMap() {
-        var mapDiv = document.getElementById('map-image');
-        var map = new google.maps.Map(mapDiv, {
-            center: {lat: 44.540, lng: -78.546},
-            zoom: 8
-        });
-      }
+ 
+
+  function initMap() {
+
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 44.540, lng: -78.546},
+        zoom: 8
+    });
+
+  }
+
