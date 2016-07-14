a032e9fd564a479773e5019ef69ae43f55906abd
Munene Kaumbutho <Munene Kaumbutho>
2016-07-14 00:21:57 +0000

    managed to place a marker on map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -21,9 +21,38 @@ $(document).ready( function(){
 
     var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, {
-        center: {lat: 44.540, lng: -78.546},
-        zoom: 8
+        center: {lat: 49.2827, lng: -123.1207},
+        zoom: 13,
+        zoomControl: true,
+        scaleControl: true,
+
+        //map type:
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+
+        // map type controls:
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+          position: google.maps.ControlPosition.TOP_CENTER
+        },
+
+        //map zoom controls:
+        zoomControl: true,
+        zoomControlOptions: {
+          position: google.maps.ControlPosition.RIGHT_BOTTOM
+        },
+
+
     });
+
+    var marker = new google.maps.Marker({
+        position: {lat: 49.2827, lng: -123.1207},
+        map: map,
+        title: 'Hello World!'
+    });
+
+
+
 
   }
 
