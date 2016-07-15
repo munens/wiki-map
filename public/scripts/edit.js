1bd988186b88b907d95ecf3a005ab3045d8220d2
Munene Kaumbutho <Munene Kaumbutho>
2016-07-15 21:28:45 +0000

    added windows to every existing pin

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -11,7 +11,33 @@ $(document).ready(function(){
 
 function addPinsToMap(map, pins){
     
+	
+		var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+  
+
+
     for(var key in pins){
+
+      
 
       var pin = new google.maps.Marker({
         position: {lat: pins[key].latitude, lng: pins[key].longitude},
@@ -19,6 +45,14 @@ function addPinsToMap(map, pins){
       });
 
       pin.setMap(map);
+
+      pin.addListener('click', function(e) {
+      	var infowindow = new google.maps.InfoWindow({
+      		content : contentString
+     	 })
+      	infowindow.open(map, this);
+      })
+    
     }
 }
 
@@ -88,6 +122,22 @@ function initMap(map){
     });
 
 
+
+
+
+
+
+
+    // var infowindow = new google.maps.InfoWindow({
+    //   content:
+    // });
+
+
+
+
+    pin.addListener('double click', function(){
+
+    })
     
   }
 
